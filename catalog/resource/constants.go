6bd11692395d144c18ef155ae514eeb41141d858
Alexandr Krylovskiy <alexandr.krylovskiy@gmail.com>
2014-09-29 16:33:44 +0200

    RC api version 0.1.0 (before release)

--- catalog/resource/constants.go
+++ catalog/resource/constants.go
@@ -3,7 +3,7 @@ package device
 const (
 	DnssdServiceType  = "_linksmart-rc._tcp"
 	MaxPerPage        = 100
-	ApiVersion        = "1.0.0"
+	ApiVersion        = "0.1.0"
 	ApiCollectionType = "ResourceCatalog"
 	ApiDeviceType     = "Device"
 	ApiResourceType   = "Resource"
