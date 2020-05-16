0a21ebb1579a5345b1953d1ce15b1ce6dd8022f1
Farshid Tavakolizadeh <email@farshid.ws>
2020-05-16 13:23:51 +0200

    removed misplaced test function

--- wot/validation_test.go
+++ wot/validation_test.go
@@ -59,30 +59,4 @@ func TestValidateMap(t *testing.T) {
 			t.Fatalf("Didn't return error on missing mandatory title.")
 		}
 	})
-
-	t.Run("non-float TTL", func(t *testing.T) {
-		var td = map[string]any{
-			"@context": "https://www.w3.org/2019/wot/td/v1",
-			"id":       "urn:example:test/thing1",
-			"title":    "example thing",
-			"security": []string{"basic_sc"},
-			"securityDefinitions": map[string]any{
-				"basic_sc": map[string]string{
-					"in":     "header",
-					"scheme": "basic",
-				},
-			},
-			"ttl": 1,
-		}
-		err := ValidateMap(&td)
-		if err == nil {
-			t.Fatalf("Didn't return error on integer TTL.")
-		}
-
-		td["ttl"] = "1"
-		err = ValidateMap(&td)
-		if err == nil {
-			t.Fatalf("Didn't return error on string TTL.")
-		}
-	})
 }
