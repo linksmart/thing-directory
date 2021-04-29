cf90e2f399267263c191d98f73a85568b8e4138e
Farshid Tavakolizadeh <email@farshid.ws>
2021-04-29 15:50:11 +0200

    Add dns-sd types to wot package

---
+++ wot/discovery.go
@@ -0,0 +1,11 @@
+package wot
+
+const (
+	// Media Types
+	MediaTypeJSONLD = "application/ld+json"
+	MediaTypeJSON   = "application/ld+json"
+	// DNS-SD Types
+	DNSSDServiceType             = "_wot._tcp"
+	DNSSDServiceSubtypeThing     = "_thing"     // _thing._sub._wot._tcp
+	DNSSDServiceSubtypeDirectory = "_directory" // _directory._sub._wot._tcp
+)
